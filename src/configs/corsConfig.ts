5839419daa9181716fa25e0b2cf68351bb4f35bb
codecreatorsss <codecreatorsss@gmail.com>
2024-08-31 17:55:36 +0530

    added new cors option

--- src/configs/corsConfig.ts
+++ src/configs/corsConfig.ts
@@ -1,4 +1,7 @@
-export const whitelist = ["http://localhost:5173"];
+export const whitelist = [
+  "http://localhost:5173",
+  "https://js-interview-prep.netlify.app",
+];
 
 // Configure CORS options
 export const corsOptions = {
