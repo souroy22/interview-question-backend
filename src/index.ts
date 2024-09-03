2a06574fe2b5e124d0a764442c00c555c6f77ff8
codecreatorsss <codecreatorsss@gmail.com>
2024-09-03 15:31:51 +0530

    changed default port

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { PaginationOptions } from "./utils/pagination";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || "8000";
+const PORT = process.env.PORT || "3000";
 
 declare global {
   namespace Express {
